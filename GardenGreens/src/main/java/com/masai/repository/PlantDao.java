3551d2c8035ab85e5acf677c755505c7adaa791d
Jyoti Pardhi <105926905+JyotiP123@users.noreply.github.com>
2022-12-15 22:31:58 +0530

    Add files via upload

---
+++ GardenGreens/src/main/java/com/masai/repository/PlantDao.java
@@ -0,0 +1,18 @@
+package com.masai.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masai.model.Plants;
+
+@Repository
+public interface PlantDao extends JpaRepository<Plants, Integer> {
+	
+	public Optional<Plants> findByPlantName(String plantName);
+	
+	public List<Plants> findByPlantType(String plantType);
+
+}
