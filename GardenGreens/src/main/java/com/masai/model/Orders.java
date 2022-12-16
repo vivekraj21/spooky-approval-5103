7368928b4aa4d8a141be0d1cb2c61bee0328070e
Mrityunjay Sarkar <mrityunjaysorcar@gmail.com>
2022-12-17 00:06:30 +0530

    added relationship order planters

--- GardenGreens/src/main/java/com/masai/model/Orders.java
+++ GardenGreens/src/main/java/com/masai/model/Orders.java
@@ -1,12 +1,17 @@
 package com.masai.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,22 +29,10 @@ public class Orders {
 	private String transactionMode;
 	private Integer quantity;
 	private Double cost;
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Orders other = (Orders) obj;
-		return Objects.equals(cost, other.cost) && Objects.equals(orderDate, other.orderDate)
-				&& Objects.equals(quantity, other.quantity) && Objects.equals(transactionMode, other.transactionMode);
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(cost, orderDate, quantity, transactionMode);
-	}
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "orderId")
+	private Set<Planter> planters = new HashSet<>();
 	
 	
 	
